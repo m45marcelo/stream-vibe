fff4b3f9f5d477b15168049d6e59f87e0d495d53
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-11-09 16:36:33 -0300

    feat: criado o componente Footer

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return(
+        <footer className="w-full h-103.5 bg-black-6">
+            <div className="h-[10.8125rem] w-full flex justify-between">
+
+            </div>
+        </footer>
+    )
+}
